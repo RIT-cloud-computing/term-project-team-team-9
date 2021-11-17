bd8f3bb32070aa23dac2c0de31379e775275b82a
Brian Mirabito <bjm9265@g.rit.edu>
2021-11-17 18:55:32 -0500

    some stubs for IAC

--- main.tf
+++ main.tf
@@ -23,3 +23,38 @@ resource "aws_instance" "video_stream_ec2" {
     Name = "VideoStreamInstance"
   }
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "jaywalker-s3-bucket"
+  acl    = "private"
+}
+
+resource "aws_sns_topic" "sns_emails" {
+  name = "sns-notif"
+}
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 3.0"
+
+  identifier = "offending-db"
+
+  engine            = "mysql"
+  engine_version    = "8.0.23"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 20
+
+  name     = "offending-db"
+  username = "admin"
+  password = "YourPwdShouldBeLongAndSecure!"
+  port     = "3306"
+
+  # DB subnet group
+  subnet_ids = []
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+}
