b679cb40c45fbfc9f48ff74b2a7f45cab399c6a5
Patrick Dalesio <35547072+pxd5295@users.noreply.github.com>
2021-12-02 01:09:35 -0500

    added to startup

--- SETUP.sh
+++ SETUP.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 yum update -y
+yum install git
 
 #pull from github
+git clone https://github.com/RIT-cloud-computing/term-project-team-team-9.git
 
 #run script
+cd term-project-team-team-9
 
 #print
 echo "Completed"
