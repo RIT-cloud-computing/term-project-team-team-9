e2971326b57e0b90f1228e5b1a29fa5b497dbf3e
Patrick Dalesio <35547072+pxd5295@users.noreply.github.com>
2021-12-02 02:02:26 -0500

    added deploy

--- SETUP.sh
+++ SETUP.sh
@@ -6,7 +6,13 @@ yum install git
 git clone https://github.com/RIT-cloud-computing/term-project-team-team-9.git
 
 #run script
-cd term-project-team-team-9
+cd term-project-team-team-9/cdk
 
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install -r requirements.txt
+
+cdk deploy
 #print
 echo "Completed"
