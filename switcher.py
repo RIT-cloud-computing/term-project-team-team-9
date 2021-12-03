34dcc376e568be846db257a5e80481b84c3b3043
Jacob Scida <scidaj@gmail.com>
2021-12-02 22:42:50 -0500

    Grabber to move values around.

---
+++ switcher.py
@@ -0,0 +1,27 @@
+import boto3
+import fileinput
+import json
+
+def main():
+    cdkOutput = open("/cdk/cdk-outputs.json")
+
+    cdkVars = json.load(cdkOutput)
+
+    buffer_bucket = cdkVars["BufferBucket"]
+    site_bucket = cdkVars["names"]["HTMLBucket"]
+    endpoint = cdkVars["names"]["API"]
+    
+    with fileinput.FileInput("./index.html", inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace({APIEndPoint}, endpoint), end='')
+    
+    with fileinput.FileInput("./yt_grabber/grabber.py", inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace('rek-image-buffer', buffer_bucket), end='')
+
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('./index.html', site_bucket, 'index.html')
+
+
+if __name__ == "__main__":
+    main()
